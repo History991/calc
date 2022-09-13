7681a5bb837b8f8c03b610ab12415856c1cb0457
daniel r <smartworldbiglife@gmail.com>
2022-09-13 18:26:18 +0200

    agregar "aproximadamente" a los resultados

--- energia/gas.sh
+++ energia/gas.sh
@@ -7,10 +7,10 @@
 echo "Introduce el valor de m3"
 read m3
 kwh=$(echo "scale=2; $m3 * 11.70" | bc)
-echo "El valor de $m3 m3 es $kwh kwh"
+echo "El valor de $m3 m3 es $kwh kwh aproximadamente"
 #calculamos el valor de kwh a m3
 echo "Introduce el valor de kwh"
 read kwh
 m3=$(echo "scale=2; $kwh * 0.8547008547008" | bc)
-echo "El valor de $kwh kwh es $m3 m3"
+echo "El valor de $kwh kwh es $m3 m3 aproximadamente"
 
